5b5c64f9bbff2dbb5ef0f43a2aef1f64546f4e7c
zhufengdaaa <zhufengdaaa@gmail.com>
2015-11-07 23:25:03 +0800

    multiprocess spider

--- Spider_b.py
+++ Spider_b.py
@@ -5,25 +5,27 @@ from User import User
 from DataBase import DataBase
 from Queue import Queue
 
+user = 'root'
+host = 'localhost'
+password = ''
+dbname = 'zhihu'
+db = DataBase(user, host, password, dbname)
+queue = Queue()
 
-def wow(inituser):
-    user = 'root'
-    host = 'localhost'
-    password = ''
-    dbname = 'zhihu'
-    db = DataBase(user, host, password, dbname)
-    queue = Queue()
-    queue.put(inituser)
+def run(queue):
     while not queue.empty():
         user = queue.get()
         for follower in user.get_followers():
             queue.put(follower)
-        db.put_user_in_db(user)
-        db.put_follow_user_in_db(user)
-        db.put_user_ask_in_db(user)
-        db.put_user_answer_in_db(user)
+        get_info(user)
 
+def get_info(user):
+    db.put_user_in_db(user)
+    db.put_follow_user_in_db(user)
+    db.put_user_ask_in_db(user)
+    db.put_user_answer_in_db(user)
 
 if __name__ == '__main__':
     user = User(u"http://www.zhihu.com/people/Fooying")
-    wow(user)
+    queue.put(user)
+    run(queue)
