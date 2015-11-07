74be440bd91913293cbae9fbc321bb6320a11ba6
Ulden <manjuulden@hotmail.com>
2015-11-08 02:59:14 +0800

    ReadData 写完了

---
+++ ReadData.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-_
+_author__ = 'ZombieGroup'
+
+import MySQLdb
+
+
+class ReadData:
+    def __init__(self, user=None, host=None, password=None, dbname=None):
+        self.connect = MySQLdb.connect(host, user, password, dbname, port = 3306, charset = 'utf8')
+
+    def read_from_user(self, user_id):
+        connect = self.connect
+        cursor = connect.cursor()
+        cursor.execute('select * from Users where user_id="%s"' % user_id)
+        return cursor.fetchone()
+
+    def followers_of_user_question(self, user_id):
+        connect = self.connect
+        cursor = connect.cursor()
+
+        cursor.execute(
+            'select follower_id from Follow_Question where question_id in (select question_id from Question where '
+            'asker_id="%s")' % user_id)
+        return cursor.fetchall()
+
+    def followers_of_user(self, user_id):
+        connect = self.connect
+        cursor = connect.cursor()
+
+        cursor.execute('select follower_id from Follow_User where followee_id="%s"' % user_id)
+
+        return cursor.fetchall()
+
+    def voters_of_user_answer(self, user_id):
+        connect = self.connect
+        cursor = connect.cursor()
+
+        cursor.execute(
+            'select voter_id from Vote_Answer where answer_id in (select answer_id from Answers where '
+            'author_id="%s")' % user_id)
+
+        return cursor.fetchall()
--- test_database.py
+++ test_database.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ZombieGroup'
 from DataBase import DataBase
+from ReadData import ReadData
 from User import User
 from Question import Question
 
@@ -11,11 +12,21 @@ def main():
     password = ''
     dbname = 'zhihu'
     database = DataBase(user, host, password, dbname)
+    readdata = ReadData(user, host, password, dbname)
     user_msg = User(u"http://www.zhihu.com/people/Fooying")
     question_msg = Question(u"http://www.zhihu.com/question/31918396")
 
-    database.put_user_in_db(user_msg)
-    database.put_follow_user_in_db(user_msg)
+    '''user_id, follower_num, followee_num, vote_num, thanks_num, ask_num, answer_num, article_num, collection_num, \
+    following_topic_num, following_column_num, education, education_extra, location, business, position, employment =\
+        readdata.read_from_user(
+        user_msg.get_user_id())
+    value = (
+        user_id, follower_num, followee_num, vote_num, thanks_num, ask_num, answer_num, article_num, collection_num,
+        following_topic_num, following_column_num, education, education_extra, location, business, position, employment)
+    print value'''
+    
+    # database.put_user_in_db(user_msg)
+    # database.put_follow_user_in_db(user_msg)
 
     # for user in user_msg.get_followers():
     #   database.put_follow_user_in_db(user)
@@ -24,5 +35,7 @@ def main():
     # database.put_user_ask_in_db(user_msg)
 
     # database.put_question_in_db(question_msg)
+
+
 if __name__ == '__main__':
     main()
