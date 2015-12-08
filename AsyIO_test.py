95577497c8aefd29db512a2131702dfae0d2f6b7
Ulden <manjuulden@hotmail.com>
2015-12-08 14:54:16 +0800

    微调

--- AsyIO_test.py
+++ AsyIO_test.py
@@ -22,12 +22,12 @@ def question_slave(name):
     try:
         while True:
             question = question_queue.get()
-            print 'slave %s get one question from the queue' % name
+            print 'question slave %s get one question from the queue' % name
             database.put_question_in_db(question)
             question.get_followers()
             question.get_answers()
     except Empty:
-        print 'slave %s is dead' % name
+        print 'question slave %s is dead' % name
         gevent.sleep(0)
 
 
@@ -35,11 +35,11 @@ def answer_slave(name):
     try:
         while True:
             answer = answer_queue.get()
-            print 'slave %s get one answer from the queue' % name
+            print 'answer slave %s get one answer from the queue' % name
             database.put_answer_in_db(answer)
             answer.get_upvoters()
     except Empty:
-        print 'slave %s is dead' % name
+        print 'answer slave %s is dead' % name
         gevent.sleep(0)
 
 
@@ -47,23 +47,22 @@ def user_slave(name):
     try:
         while True:
             user = user_queue.get()
-            print 'slave %s get one user from the queue' % name
+            print 'user slave %s get one user from the queue' % name
             database.put_user_in_db(user)
             user.get_followers()
             user.get_answers()
             user.get_asks()
     except Empty:
-        print 'slave %s is dead' % name
+        print 'user slave %s is dead' % name
         gevent.sleep(0)
 
 
 def master(name):
-    print 'master %s'%name
+    print 'master %s' % name
     topic.get_questions()
 
 masters = [gevent.spawn(master, i) for i in xrange(10)]
-users = [gevent.spawn(user_slave,i) for i in xrange(100)]
-questions = [gevent.spawn(question_slave,i) for i in xrange(100)]
-answers = [gevent.spawn(answer_slave,i) for i in xrange(100)]
-gevent.joinall(masters+users+questions+answers)
-
+users = [gevent.spawn(user_slave, i) for i in xrange(100)]
+questions = [gevent.spawn(question_slave, i) for i in xrange(100)]
+answers = [gevent.spawn(answer_slave, i) for i in xrange(100)]
+gevent.joinall(masters + users + questions + answers)
