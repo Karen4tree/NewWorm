be820a1563181d852d52f3e9a968cae2895ade0d
王肇一 <manjuulden@hotmail.com>
2016-01-19 18:27:39 +0800

    登陆问题解决

--- zhihu_api/Logging.py
+++ zhihu_api/Logging.py
@@ -11,27 +11,22 @@ class Logging:
 
     @staticmethod
     def error(msg):
-        if Logging.flag:
-            print "".join([termcolor.colored("ERROR", "red"), ": ", termcolor.colored(msg, "white")])
-
+        if Logging.flag == True:
+            print "".join(  [ termcolor.colored("ERROR", "red"), ": ", termcolor.colored(msg, "white") ] )
     @staticmethod
     def warn(msg):
-        if Logging.flag:
-            print "".join([termcolor.colored("WARN", "yellow"), ": ", termcolor.colored(msg, "white")])
-
+        if Logging.flag == True:
+            print "".join(  [ termcolor.colored("WARN", "yellow"), ": ", termcolor.colored(msg, "white") ] )
     @staticmethod
     def info(msg):
         # attrs=['reverse', 'blink']
-        if Logging.flag:
-            print "".join([termcolor.colored("INFO", "magenta"), ": ", termcolor.colored(msg, "white")])
-
+        if Logging.flag == True:
+            print "".join(  [ termcolor.colored("INFO", "magenta"), ": ", termcolor.colored(msg, "white") ] )
     @staticmethod
-    def debug(msg, value):
-        if Logging.flag and debug_info_flag:
-            print "".join([termcolor.colored("DEBUG", "magenta"), ": ", termcolor.colored(msg, "white"), "  ",
-                           termcolor.colored(value, "white")])
-
+    def debug(msg):
+        if Logging.flag == True:
+            print "".join(  [ termcolor.colored("DEBUG", "magenta"), ": ", termcolor.colored(msg, "white") ] )
     @staticmethod
     def success(msg):
-        if Logging.flag:
-            print "".join([termcolor.colored("SUCCES", "green"), ": ", termcolor.colored(msg, "white")])
+        if Logging.flag == True:
+            print "".join(  [ termcolor.colored("SUCCES", "green"), ": ", termcolor.colored(msg, "white") ] )
--- zhihu_api/Login.py
+++ zhihu_api/Login.py
@@ -5,15 +5,18 @@ import re
 import platform
 import random
 import termcolor
+import sys
+import json
 
 from __init__ import *
 from Exceptions import *
+
 __author__ = 'ZombieGroup'
 
 
 def download_captcha():
     url = "http://www.zhihu.com/captcha.gif"
-    r = requests.get(url, params={"r": random.random()})
+    r = requests.get(url, params = {"r": random.random()})
     if int(r.status_code) != 200:
         raise NetworkError(u"验证码请求失败")
     image_name = u"verify." + r.headers['content-type'].split("/")[1]
@@ -39,12 +42,12 @@ def download_captcha():
     elif platform.system() == "NetBSD":
         os.system("open %s &" % image_name)
     elif platform.system() == "Windows":
-        os.system("open %s &" % image_name)
+        os.system("%s" % image_name)
     else:
-        Logging.info(u"我们无法探测你的作业系统，请自行打开验证码 %s 文件，并输入验证码。" %
-                     os.path.join(os.getcwd(), image_name))
+        Logging.info(u"我们无法探测你的作业系统，请自行打开验证码 %s 文件，并输入验证码。" % os.path.join(os.getcwd(), image_name))
 
-    captcha_code = raw_input(termcolor.colored("请输入验证码: ", "cyan"))
+    sys.stdout.write(termcolor.colored(u"请输入验证码: ", "cyan"))
+    captcha_code = raw_input()
     return captcha_code
 
 
@@ -53,8 +56,7 @@ def search_xsrf():
     r = requests.get(url)
     if int(r.status_code) != 200:
         raise NetworkError(u"验证码请求失败")
-    results = re.compile(
-        r"<input\stype=\"hidden\"\sname=\"_xsrf\"\svalue=\"(\S+)\"", re.DOTALL).findall(r.text)
+    results = re.compile(r"\<input\stype=\"hidden\"\sname=\"_xsrf\"\svalue=\"(\S+)\"", re.DOTALL).findall(r.text)
     if len(results) < 1:
         Logging.info(u"提取XSRF 代码失败")
         return None
@@ -62,8 +64,8 @@ def search_xsrf():
 
 
 def build_form(account, password):
-    if re.match(r"^\d{11}$", account):
-        account_type = "phone"
+    if re.match(r"^1\d{10}$", account):
+        account_type = "phone_num"
     elif re.match(r"^\S+@\S+\.\S+$", account):
         account_type = "email"
     else:
@@ -76,18 +78,30 @@ def build_form(account, password):
 
 
 def upload_form(form):
-    url = "http://www.zhihu.com/login/email"
+    if "email" in form:
+        url = "http://www.zhihu.com/login/email"
+    elif "phone_num" in form:
+        url = "http://www.zhihu.com/login/phone_num"
+    else:
+        raise ValueError(u"账号类型错误")
+
     headers = {
         'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 "
                       "Safari/537.36", 'Host': "www.zhihu.com", 'Origin': "http://www.zhihu.com", 'Pragma': "no-cache",
         'Referer': "http://www.zhihu.com/", 'X-Requested-With': "XMLHttpRequest"}
 
-    r = requests.post(url, data=form, headers=headers)
+    r = requests.post(url, data = form, headers = headers)
     if int(r.status_code) != 200:
         raise NetworkError(u"表单上传失败!")
 
     if r.headers['content-type'].lower() == "application/json":
-        result = r.json()
+        try:
+            result = json.loads(r.content)
+        except Exception as e:
+            Logging.error(u"JSON解析失败！")
+            Logging.debug(e)
+            Logging.debug(r.content)
+            result = {}
         if result["r"] == 0:
             Logging.success(u"登录成功！")
             return {"result": True}
@@ -104,17 +118,17 @@ def upload_form(form):
 
 def islogin():
     # check session
-    url = "http://www.zhihu.com/settings/profile"
-    r = requests.get(url, allow_redirects=False)
+    url = "https://www.zhihu.com/settings/profile"
+    r = requests.get(url, allow_redirects = False)
     status_code = int(r.status_code)
-    flag = False
     if status_code == 301 or status_code == 302:
-        raise NotLogin
+        # 未登录
+        return False
     elif status_code == 200:
-        flag = True
-        return flag
+        return True
     else:
-        raise NetworkError
+        Logging.warn(u"网络故障")
+        return None
 
 
 def read_account_from_config_file(config_file="config.ini"):
@@ -136,37 +150,40 @@ def read_account_from_config_file(config_file="config.ini"):
             return email, password
     else:
         Logging.error(u"配置文件加载失败！")
-        return None, None
+        return (None, None)
 
 
 def login(account=None, password=None):
-    try:
-        islogin()
-    except NotLogin, e:
+    if islogin():
+        Logging.success(u"你已经登录过咯")
+        return True
 
-        if account is None:
-            (account, password) = read_account_from_config_file()
-        if account is None:
-            account = raw_input("请输入登录帐号: ")
-            password = raw_input("请输入登录密码: ")
+    if account is None:
+        (account, password) = read_account_from_config_file()
+    if account is None:
+        sys.stdout.write(u"请输入登录账号: ")
+        account = raw_input()
+        sys.stdout.write(u"请输入登录密码: ")
+        password = raw_input()
 
-        form_data = build_form(account, password)
-        """
-            result:
-                {"result": True}
-                {"error": {"code": 19855555, "message": "unknow.", "data": "data" } }
-                {"error": {"code": -1, "message": u"unknow error"} }
-        """
-        result = upload_form(form_data)
-        if "error" in result:
-            if result['error']['code'] == 1991829:
-                # 验证码错误
-                Logging.error(u"验证码输入错误，请准备重新输入。")
-                return login(account, password)
-            else:
-                Logging.warn(u"unknow error.")
-                return False
-        elif "result" in result and result['result'] == True:
-            Logging.success(u"登录成功！")
-            requests.cookies.save('./cookies')
-            return True
+    form_data = build_form(account, password)
+    """
+        result:
+            {"result": True}
+            {"error": {"code": 19855555, "message": "unknow.", "data": "data" } }
+            {"error": {"code": -1, "message": u"unknow error"} }
+    """
+    result = upload_form(form_data)
+    if "error" in result:
+        if result["error"]['code'] == 1991829:
+            # 验证码错误
+            Logging.error(u"验证码输入错误，请准备重新输入。")
+            return login()
+        else:
+            Logging.warn(u"unknow error.")
+            return False
+    elif "result" in result and result['result'] == True:
+        # 登录成功
+        Logging.success(u"登录成功！")
+        requests.cookies.save()
+        return True
