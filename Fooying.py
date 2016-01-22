d8263a2db26567f4eedffc6965b4f099a440359b
王肇一 <manjuulden@hotmail.com>
2016-01-23 00:09:31 +0800

    adjustment

--- Fooying.py
+++ Fooying.py
@@ -3,13 +3,12 @@
 from database_operation.DataBase import DataBase
 from zhihu_api.Topic import Topic
 from zhihu_api import questionBloom, userBloom, userQueue, answerBloom, topicBloom, topicQueue
+from zhihu_api.Logging import Logging
 # http://stackoverflow.com/questions/5318936/python-multiprocessing-pool-lazy-iteration
 import multiprocessing as mp
 import itertools
 
 __author__ = 'ZombieGroup'
-
-topic = Topic("https://www.zhihu.com/topic/19554927")
 
 
 def spider(questions):
@@ -17,18 +16,19 @@ def spider(questions):
         questionBloom.insert_element(question.get_question_id())
         DataBase.put_question_in_db(question)
 
+        Logging.info("Follower of question id %s" % question.get_question_id())
         for follower in question.get_followers():
             if not userBloom.is_element_exist(follower.get_user_id()):
                 userBloom.insert_element(follower.get_user_id())
-                userQueue.put(follower)
+                #userQueue.put(follower)
                 DataBase.put_user_in_db(follower)
             DataBase.put_follow_question_in_db(question, follower)
-
+        Logging.info("Topics of question id %s" % question.get_question_id())
         for topic in question.get_topics():
             if not topicBloom.is_element_exist(topic.get_topic_id()):
                 topicBloom.insert_element(topic.get_topic_id())
             DataBase.put_question_topic_in_db(question, topic)
-
+        Logging.info("Answers of question id %s"%question.get_question_id())
         for answer in question.get_answers():
             if not answerBloom.is_element_exist(answer.get_answer_id()):
                 answerBloom.insert_element(answer.get_answer_id())
