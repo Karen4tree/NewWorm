1c7ae7995efa60a19842f6f1b584a7b1f9ccf6fb
王肇一 <manjuulden@hotmail.com>
2016-01-30 15:14:17 +0800

    多线程写成单线程了我的锅wq

--- Fooying.py
+++ Fooying.py
@@ -8,45 +8,47 @@ from zhihu_api.Logging import Logging
 # http://stackoverflow.com/questions/5318936/python-multiprocessing-pool-lazy-iteration
 import multiprocessing as mp
 import itertools
+import time
 
 __author__ = 'ZombieGroup'
 
 
-def spider(questions):
-    for question in questions:
-        questionBloom.insert_element(question.get_question_id())
-        DataBase.put_question_in_db(question)
+def spider(question):
+    questionBloom.insert_element(question.get_question_id())
+    DataBase.put_question_in_db(question)
 
-        Logging.info("Follower of question id %s" % question.get_question_id())
-        for follower in question.get_followers():
-            if not userBloom.is_element_exist(follower.get_user_id()):
-                userBloom.insert_element(follower.get_user_id())
-                DataBase.put_user_in_db(follower)
-                for userfollower in follower.get_followers():
-                    if not userBloom.is_element_exist(userfollower.get_user_id()):
-                        userBloom.insert_element(userfollower.get_user_id())
-                        DataBase.put_user_in_db(userfollower)
-                    DataBase.put_follow_user_in_db(follower,userfollower)
-            DataBase.put_follow_question_in_db(question, follower)
+    Logging.info("Follower of question id %s" % question.get_question_id())
+    for follower in question.get_followers():
+        if not userBloom.is_element_exist(follower.get_user_id()):
+            userBloom.insert_element(follower.get_user_id())
+            DataBase.put_user_in_db(follower)
+            for userfollower in follower.get_followers():
+                if not userBloom.is_element_exist(userfollower.get_user_id()):
+                    userBloom.insert_element(userfollower.get_user_id())
+                    DataBase.put_user_in_db(userfollower)
+                DataBase.put_follow_user_in_db(follower,userfollower)
+        DataBase.put_follow_question_in_db(question, follower)
 
-        Logging.info("Topics of question id %s" % question.get_question_id())
-        for topic in question.get_topics():
-            if not topicBloom.is_element_exist(topic.get_topic_id()):
-                topicBloom.insert_element(topic.get_topic_id())
-            DataBase.put_question_topic_in_db(question, topic)
+    Logging.info("Topics of question id %s" % question.get_question_id())
+    for topic in question.get_topics():
+        if not topicBloom.is_element_exist(topic.get_topic_id()):
+            topicBloom.insert_element(topic.get_topic_id())
+        DataBase.put_question_topic_in_db(question, topic)
 
-        Logging.info("Answers of question id %s"%question.get_question_id())
-        for answer in question.get_answers():
-            if not answerBloom.is_element_exist(answer.get_answer_id()):
-                answerBloom.insert_element(answer.get_answer_id())
-            if not userBloom.is_element_exist(answer.get_author_id()):
-                DataBase.put_user_in_db(User("http://www.zhihu.com/people/%s" % answer.get_author_id()))
-                userBloom.insert_element(answer.get_author_id())
-            DataBase.put_answer_in_db(answer)
-            for user in answer.get_upvoters():
-                if not userBloom.is_element_exist(user.get_user_id()):
-                    userBloom.insert_element(user.get_user_id())
-                DataBase.put_vote_in_db(answer, user)
+    Logging.info("Answers of question id %s"%question.get_question_id())
+    for answer in question.get_answers():
+        if not answerBloom.is_element_exist(answer.get_answer_id()):
+            answerBloom.insert_element(answer.get_answer_id())
+        if not userBloom.is_element_exist(answer.get_author_id()):
+            DataBase.put_user_in_db(User("http://www.zhihu.com/people/%s" % answer.get_author_id()))
+            userBloom.insert_element(answer.get_author_id())
+        DataBase.put_answer_in_db(answer)
+        for user in answer.get_upvoters():
+            if not userBloom.is_element_exist(user.get_user_id()):
+                userBloom.insert_element(user.get_user_id())
+            DataBase.put_vote_in_db(answer, user)
+
+    time.sleep(0)
 
 
 if __name__ == '__main__':
@@ -58,6 +60,6 @@ if __name__ == '__main__':
     go = topic.get_questions()
     N = 20
     while True:
-        g2 = p.map(spider(itertools.islice(go, N)))
+        g2 = p.map(spider,itertools.islice(go, N))
         if not g2:
             break
