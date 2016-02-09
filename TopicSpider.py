cad30b59654cfee3b6d270fff476a10dc7e923b1
王肇一 <manjuulden@hotmail.com>
2016-02-10 02:37:56 +0800

    爬话题树

--- TopicSpider.py
+++ TopicSpider.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from zhihu_api.Topic import Topic
+from database_operation.DataBase import DataBase
 
-if __name__ == '__main__':
-    THREADS = 8
-    p = mp.Pool(THREADS)
-    topic = Topic("http://www.zhihu.com/topic/19736651")
-    if not topicBloom.is_element_exist(topic.get_topic_id()):
-        topicBloom.insert_element(topic.get_topic_id())
-        Worm_status.record_status("topicBloom", topicBloom)
-        DataBase.put_topic_in_db(topic)
-    go = topic.get_questions()
-    N = 20
-    while True:
+
+def store(topic):
+    if topic is not None:
+        for item in topic.get_child():
+            store(item)
         try:
-            p.map_async(spider, itertools.islice(go, N))
-        except TypeError:
-            continue
+            DataBase.put_topic_in_db(topic)
+            print topic.get_topic_name()
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    import sys
+    sys.setrecursionlimit(1000000)
+    rootTopic = Topic("http://www.zhihu.com/topic/19776749")
+    store(rootTopic)
